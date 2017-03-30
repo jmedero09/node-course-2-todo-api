003c0516bd73e50c606c236f37e6c0f1f63d29ac
Jesus Medero <jmedero09@gmail.com>
2017-03-30 02:42:43 -0400

    add test for /todos

--- server/server.js
+++ server/server.js
@@ -17,11 +17,11 @@ app.post('/todos', (req, res) => {
         res.send(doc);
     }, (e) => {
         res.status(400).send(e);
-    })
+    });
 });
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send(todos);
+        res.send({ todos });
     }, (e) => {
         res.status(400).send(e)
     });
--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,10 +4,20 @@ const request = require('supertest');
 const { app } = require('./../server.js');
 const { Todo } = require('./../modles/todo');
 
+const todos = [{
+        text: 'first test todo'
+    },
+    {
+        text: 'second text todo'
+    }
+]
+
+
+//this deletes our todos in the database before we test it 
 beforeEach((done) => {
     Todo.remove({}).then(() => {
-        done()
-    })
+        return Todo.insertMany(todos)
+    }).then(() => done())
 });
 
 
@@ -26,7 +36,7 @@ describe('POST/todos', () => {
                 if (err) {
                     return done(err)
                 }
-                Todo.find().then((todos) => {
+                Todo.find({ text }).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done()
@@ -45,7 +55,7 @@ describe('POST/todos', () => {
                     return done(err);
                 }
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch((e) => {
                     done(e);
@@ -54,3 +64,15 @@ describe('POST/todos', () => {
 
     });
 });
+
+describe('GET/todos route', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2)
+            })
+            .end(done);
+    })
+})
