05ced019a9c0a6b6762377499e0af8b5e9dea71a
Jesus Medero <jmedero09@gmail.com>
2017-03-30 20:03:40 -0400

    add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ var { Todo } = require('./modles/todo');
 var { User } = require('./modles/user');
 var express = require('express');
 var bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 
 var app = express();
@@ -26,6 +27,27 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e)
     });
 })
+
+app.get('/todos/:id', (req, res) => {
+    //res.send(req.params);
+
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send(todo)
+    }).catch((e) => {
+        res.status(400).send();
+    })
+
+})
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
